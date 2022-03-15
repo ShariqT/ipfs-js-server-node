ae56b3614dbb29f487571f2c9aae3679d170bedc
Shariq Torres <shariq.torres@lob.com>
2022-03-14 18:21:44 -0700

    updated file to take out things required by heroku
    Since heroku can only open one port per project, we decided to remove the stuff in code that related to this.

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:buster
-
-RUN mkdir /app
-COPY ./ /app
-WORKDIR /app
-RUN npm install
-EXPOSE 4012
-EXPOSE 80
-ENTRYPOINT [ "node" , "server.js" ]
--- server.js
+++ server.js
@@ -4,36 +4,40 @@ const express = require("express")
   
 async function main(){
     const app = express()
-
-    app.get('/', (req, res) => {
-        res.send(__dirname + "/index.html")
-    })
-    app.listen(80, async () => {
-        console.log(`Example app listening on port 80`)
-        const node = await IPFS.create({
-            EXPERIMENTAL: {
-                pubsub: true
-            },
-            config:{
-                Addresses:{
-                Swarm: [
-                    '/ip4/0.0.0.0/tcp/4012/ws'                  
-                ],
-              }
-            },
-            relay: {
-                enabled: true,
-                hop: {
-                    enabled: true
-                }
+    const node = await IPFS.create({
+        EXPERIMENTAL: {
+            pubsub: true
+        },
+        config:{
+            Addresses:{
+            Swarm: [
+                '/ip4/0.0.0.0/tcp/4013',
+                '/ip4/0.0.0.0/tcp/4012/wss'                  
+            ],
+          }
+        },
+        relay: {
+            enabled: true,
+            hop: {
+                enabled: true
             }
-        })
+        }
+    })
+    
+
+    node.pubsub.subscribe('com.lob.www:dtwitter-poc', function(mes){
+        console.log(mes)
+    })
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + "/index.html")
+    })
+    app.get('/addr', async (req, res) => {
         const id = await node.id()
         console.log(id)
-
-        node.pubsub.subscribe('com.lob.www:dtwitter-poc', function(mes){
-            console.log(mes)
-        })
+        res.send('/dns4/projects.enshapa-engineering.com/tcp/4013/ws/p2p/' + id)
+    })
+    app.listen(45550, () => {
+        console.log(`Example app listening on port ${45550}`)
     })
     
 }
